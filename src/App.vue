027f52829223391f4932e9e70f6f12b359f50ff3
Andrei Voicu <dreivoicu@gmail.com>
2022-02-25 18:50:32 +0200

    timeout

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,14 @@ const count = 500;
 const data = Array(500).fill({ loading: true });
 const items: Ref<any[]> = ref([]);
 
+let timeout: any;
 function getData(offset: number, limit: number) {
+  clearTimeout(timeout);
   const start = Math.max(0, offset);
   const end = Math.min(offset + limit, count);
   items.value = data.slice(start, end);
 
-  setTimeout(() => {
+  timeout = setTimeout(() => {
     for (let i = offset; i < offset + limit; i++) {
       data[i] = { index: 1, text: `item ${i}` };
     }
