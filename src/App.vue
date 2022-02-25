a1c86febde8321bdfcaa246a55d4ca0dc91185a3
Andrei Voicu <dreivoicu@gmail.com>
2022-02-25 15:15:15 +0200

    trial

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <script setup lang="ts">
 import VScroll from "./components/VScroll.vue";
 
+const latency = () => Math.random() * 500 + 300;
 const count = 500;
 
-function getData(offset: number, limit: number) {
-  const data = [];
-  const start = Math.max(0, offset);
-  const end = Math.min(offset + limit - 1, count);
+async function getData(offset: number, limit: number) {
+  return new Promise((resolve) => {
+    const data: any[] = [];
+    const start = Math.max(0, offset);
+    const end = Math.min(offset + limit - 1, count);
 
-  for (let i = start; i <= end; i++) {
-    data.push({ index: i, text: `item ${i}` });
-  }
+    for (let i = start; i <= end; i++) {
+      data.push({ index: i, text: `item ${i}` });
+    }
 
-  return data;
+    setTimeout(() => resolve(data), latency());
+  });
 }
 </script>
 
--- src/components/VScroll.vue
+++ src/components/VScroll.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
-import { ref, onMounted, onUnmounted } from "vue";
-import { requestAnimationFrame, debounced } from "@/helpers";
-
+import { ref, onMounted } from "vue";
 const props = defineProps<{
   count: number;
   getData: Function;
@@ -9,52 +7,26 @@ const props = defineProps<{
 
 const { count } = props;
 const itemHeight = 20;
-const tolerance = 2;
-
-const from = ref(0);
-const viewport = ref();
-const amount = ref();
-const items = ref([]);
-const toleranceHeight = tolerance * itemHeight;
+const obs = ref();
+const data = ref([]);
+const dataLoading = ref(true);
 
 const totalHeight = count * itemHeight;
-const offsetHeight = ref(0);
+const obsHeight = 50 * itemHeight;
 
-const onScroll = requestAnimationFrame(({ target: { scrollTop } }: any) => {
-  from.value = Math.floor((scrollTop - toleranceHeight) / itemHeight);
-  const offset = amount.value + 2 * tolerance;
-
-  items.value = props.getData(from.value, offset);
-  offsetHeight.value = Math.max(from.value * itemHeight, 0);
-});
-
-const onResize = debounced(() => {
-  const height = viewport.value.clientHeight;
-  amount.value = Math.floor(height / itemHeight);
-
-  items.value = props.getData(from.value, amount.value);
-});
-const resizeObserver = new ResizeObserver(onResize);
-
-onMounted(() => {
-  resizeObserver.observe(viewport.value);
-});
-
-onUnmounted(() => {
-  resizeObserver.disconnect();
-});
+onMounted( async () => {
+  data.value = await props.getData(0, 50);
+  dataLoading.value = false;
+})
 </script>
 
 <template>
-  <div ref="viewport" :class="$style.viewport" @scroll.passive="onScroll">
+  <div :class="$style.viewport">
     <div :style="{ height: `${totalHeight}px` }">
-      <div :style="{ transform: `translateY(${offsetHeight}px)` }">
-        <div
-          :style="{ height: `${itemHeight}px` }"
-          v-for="{ index, text } in items"
-          :key="index"
-        >
-          {{ text }}
+      <div ref="obs" :style="{ height: `${obsHeight}px` }" :class="$style.obs">
+        <div v-if="dataLoading">loading...</div>
+        <div v-else>
+          <div v-for="{index, text} in data" :key="index">{{ text }}</div>
         </div>
       </div>
     </div>
@@ -64,8 +36,12 @@ onUnmounted(() => {
 <style lang="scss" module>
 .viewport {
   overflow: auto;
-  height: 100px;
+  height: 200px;
 
   resize: vertical;
+}
+
+.obs {
+  background-color: lime;
 }
 </style>
