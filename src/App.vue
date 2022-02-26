2c2c757c047f696604a191e16c8f1f8c563e58e3
Andrei Voicu <dreivoicu@gmail.com>
2022-02-26 22:42:04 +0200

    stuff

--- src/App.vue
+++ src/App.vue
@@ -6,32 +6,38 @@ import VScroll from "./components/VScroll.vue";
 const latency = () => Math.random() * 500 + 300; // simulate network latency of 300-800ms
 
 const extraItems = 100;
-const count = 5000;
+const count = 10000;
 const data: any[] = Array.from({ length: count }, (_, index) => ({ key: index }));
 const items: Ref<any[]> = ref([]);
 
 let timeout: any;
+function loadMore(start: number, end: number, direction: Direction) {
+   timeout = setTimeout(() => {
+      let from = start,
+        to = end;
+      if (direction === "down") {
+        to = Math.min(end + extraItems, count);
+      } else {
+        from = Math.max(0, start - extraItems);
+      }
+
+      for (let i = from; i < to; i++) {
+        data[i].text = `item ${i}`;
+      }
+
+      items.value = data.slice(start, end);
+    }, latency());
+}
+
 function requestItems(offset: number, limit: number, direction: Direction) {
   clearTimeout(timeout);
   const start = Math.max(0, offset);
   const end = Math.min(offset + limit, count);
   items.value = data.slice(start, end);
 
-  timeout = setTimeout(() => {
-    let from = start,
-      to = end;
-    if (direction === "down") {
-      to = Math.min(end + extraItems, count);
-    } else {
-      from = Math.max(0, offset - extraItems);
-    }
-
-    for (let i = from; i < to; i++) {
-      data[i].text = `item ${i}`;
-    }
-
-    items.value = data.slice(start, end);
-  }, latency());
+  if (items.value.find(({text}) => !text)) {
+   loadMore(start, end, direction)
+  }
 }
 </script>
 
